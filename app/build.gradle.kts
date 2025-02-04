375cc0fc2612babee6d49d483af445171a22288c
constracti <constracti@gmail.com>
2025-02-04 02:45:28 +0200

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "gr.christianikatragoudia.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.4.0"
+        versionCode = 10
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
